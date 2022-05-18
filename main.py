6863bda73ebef605c02f7b1ec53e9d2f9ed5c7b1
CCRayne <103899765+CCRayne@users.noreply.github.com>
2022-05-18 19:26:41 -0400

    Added initial CV website files

---
+++ main.py
@@ -0,0 +1,11 @@
+# Introduction to HTML
+
+# responsible for structure of the website - add image, button, textbox (foundation of all websites)
+# CSS - responsible for styling website, specify how you want the website to look (fonts, colours)
+# Javascript - allows website to actually do things/ have behaviours - functionalities
+
+# HyperText Markup Langauge (HTML)
+
+#  <br> line break/ self-closing tag
+
+# <hr horizontal tag
